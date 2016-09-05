96b3778f81202f60f56288c74888d98b413958e1
BHAUTIK04 <sens.bhautik@gmail.com>
2016-09-05 19:23:01 +0530

    dubug true

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0b+e98a8co)3hf2svf62s#1uesyjnp^5%2#z+fwu!yn60b9rsn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -113,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
